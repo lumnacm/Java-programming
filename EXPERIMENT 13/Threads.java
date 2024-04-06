9be250703a61472e2d815a3f1c6ac77e1a0370c4
lumnacm <147603018+lumnacm@users.noreply.github.com>
2024-04-06 12:07:33 +0530

    Add files via upload

---
+++ EXPERIMENT 13/Threads.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+class Multiplication extends Thread{
+public void run()
+{
+try
+{
+for(int i=0;i<=10;i++)
+{
+System.out.println(i+"x5="+(i*5));
+Thread.sleep(500);
+}
+}
+catch(InterruptedException e){
+System.out.println("Multiplication Thread Interrupted!");
+}
+}
+}
+
+
+class Prime extends Thread{
+int limit,count,flag,j,i;
+Prime(int limit)
+{
+this.limit=limit;
+}
+public void run()
+{
+try
+{ 
+int j=2;
+while(count<limit)
+{
+flag=1;
+for(i=2;i<=(j-1);i++)
+{
+if(j%i==0)
+{
+flag=0;
+break;
+}
+}
+if(flag==1)
+{
+System.out.println("Prime No:"+j);
+count ++;
+}
+j++;
+Thread.sleep(500);
+}
+}
+catch(InterruptedException e) 
+{
+System.out.print("Prime Thread Interrupted!");
+}
+}
+}
+class Threads
+{
+public static void main(String[] args)
+{
+Multiplication mul=new Multiplication();
+Scanner sc=new Scanner(System.in);
+System.out.print("Enter number of prime numbers:");
+int l=sc.nextInt();
+Prime p=new Prime(l);
+mul.start();
+p.start();
+}
+}
+
